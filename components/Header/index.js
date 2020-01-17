ce1e16892da4792125985cbc2e34b680b61b62e5
Josue Rodriguez <smexyjosue@gmail.com>
2020-01-17 08:28:31 -0800

    Familiarized myself with the HTML and wrote reuseable componentfor Header/index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,26 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+  const divHeader = document.createElement("div");
+  const spanDate = document.createElement("span");
+  const h1 = document.createElement("h1");
+  const spanTemp = document.createElement("span");
+
+  divHeader.classList.add("header");
+  spanDate.classList.add("date");
+  spanDate.textContent = "SMARCH 28, 2019";
+  h1.textContent = "Lambda Times";
+  spanTemp.classList.add("temp");
+  spanTemp.textContent = "98°";
+
+  divHeader.append(spanDate);
+  divHeader.append(h1);
+  divHeader.append(spanTemp);
+
+  return divHeader;
+}
+
+const placingHeader = document.querySelector(".header-container");
+
+placingHeader.appendChild(Header());
